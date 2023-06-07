b6b7fa04f5eeed353147ac4897385aacfa9578dc
xslyr <wesleysilv@gmail.com>
2023-06-07 13:18:09 -0300

    Add files via upload

--- lbencoder.py
+++ lbencoder.py
@@ -11,18 +11,12 @@ import numpy as np
 from sklearn.preprocessing import LabelEncoder
 
 class LbEncoder(LabelEncoder):
-	
 	def __init__(self):
 		super().__init__()
-		"""Sort Options Available:
-	- 'asc': sort by column ascending
-	- 'desc': sort by column descending
-	- 'occurrence' : sort by occurrences of data on column 
-	- ['a','c','d'] : sort by specific list ordering"""
 		self.labels = []
-		
-	
-	def get_unique_values(self, data):
+
+
+	def get_unique(self, data):
 		if isinstance(data, pd.Series):
 			return data.unique()
 		elif isinstance(data, np.ndarray) or isinstance(data, list):
@@ -39,49 +33,29 @@ class LbEncoder(LabelEncoder):
 		return current_sort
 		
 	
-	def fit(self, data, sort_by=()):
+	def fit_by_order(self, data, sort_by=()):
 		if not (isinstance(data, np.ndarray) or isinstance(data, pd.Series)):
 			raise Exception('The data parameter must be Panda Series or Numpy Array')
 		
-		if isinstance(sort_by[0], str):
-			if sort_by[0] == 'asc':
-				super().fit(data)
-			elif sort_by[0] in ['desc','occurrence']:
-				super().fit(data)
-				if sort_by[0] == 'desc':
-					aux = data.sort_values(ascending=False, kind='heapsort')
-				#self.classes_ = np.ndarray(self.get_current_sort(aux), dtype=object)
-				self.classes_ = self.get_current_sort(aux)
+		self.data = data.copy()
+		super().fit(self.data)
+		self.labels = self.classes_
+
+		if isinstance(sort_by, str):
+			if sort_by in ['desc','occurrence']:
+				self.labels = self.get_current_sort(self.data)
+				if sort_by == 'desc':
+					self.labels.sort(reverse=True)
+		elif isinstance(sort_by, list):
+			self.labels = sort_by
 		
-		elif isinstance(sort_by[0], list):
-			super().fit(data)
-			#self.classes_ = np.ndarray(sort_by[0], dtype=object)
-			self.classes_ = sort_by[0]
-		
-		#self.labels = self.classes_
-		
-		
+		self.classes_ = np.array(self.labels, dtype=object)
+
+
 	def fit_transform(self, data, sort_by=()):
 		if not (isinstance(data, np.ndarray) or isinstance(data, pd.Series)):
 			raise Exception('The data parameter must be Panda Series or Numpy Array')
 		
-		data_return = data
-		if isinstance(sort_by[0], str):
-			if sort_by[0] == 'asc':
-				data_return = super().fit_transform(data_return)
-			elif sort_by[0] in ['desc','occurrence']:
-				super().fit(data_return)
-				if sort_by[0] == 'desc':
-					aux = data_return.sort_values(ascending=False, kind='heapsort')
-				#self.classes_ = np.ndarray(self.get_current_sort(aux), dtype=object)
-				self.classes_ = self.get_current_sort(aux)
-				data_return = super().transform(data_return)
-		
-		elif isinstance(sort_by[0], list):
-			super().fit(data)
-			self.classes_ = sort_by[0]
-			data_return = super().transform(data_return)
-		
-		return data_return
-		
-
+		self.fit_by_order(data, sort_by=sort_by)
+		self.data = super().transform(self.data)
+		return self.data
